64a231a684e9d1c1fde94f0acdf75f13231bea02
mklander <mklanderyou@gmail.com>
2019-03-27 13:59:51 +0000

    move CloudwatchMonitoringClientBuilder to messaging typesafe

---
+++ messaging_typesafe/src/main/scala/uk/ac/wellcome/messaging/typesafe/CloudwatchMonitoringClientBuilder.scala
@@ -0,0 +1,15 @@
+package uk.ac.wellcome.messaging.typesafe
+
+import akka.stream.ActorMaterializer
+import com.typesafe.config.Config
+import uk.ac.wellcome.messaging.worker.monitoring.CloudwatchMonitoringClient
+import uk.ac.wellcome.monitoring.typesafe.MetricsBuilder
+
+import scala.concurrent.ExecutionContext
+
+object CloudwatchMonitoringClientBuilder {
+  def buildCloudwatchMonitoringClient(config: Config)(
+    implicit
+    materializer: ActorMaterializer,
+    ec: ExecutionContext) = new CloudwatchMonitoringClient(MetricsBuilder.buildMetricsSender(config))
+}
