c338dd58969c4775471cef3a918d3a75f5c4febc
Alex Chan <a.chan@wellcome.ac.uk>
2019-05-03 14:07:43 +0100

    Bump the storage and monitoring dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ object WellcomeDependencies {
   private lazy val versions = new {
     val fixtures = "1.0.0"
     val json = "1.1.1"
-    val monitoring = "2.1.0"
-    val storage    = "4.1.0"
+    val monitoring = "2.2.0"
+    val storage    = "4.2.0"
     val typesafe = "1.0.0"
   }
 
