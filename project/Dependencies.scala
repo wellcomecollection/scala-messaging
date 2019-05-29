8693639873c20c9f93ed7955c155c642fef21a5b
Alex Chan <a.chan@wellcome.ac.uk>
2019-05-29 14:25:11 +0100

    Bump the storage library version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object WellcomeDependencies {
     val fixtures = "1.0.0"
     val json = "1.1.1"
     val monitoring = "2.2.0"
-    val storage = "6.0.1"
+    val storage = "6.1.0"
     val typesafe = "1.0.0"
   }
 
